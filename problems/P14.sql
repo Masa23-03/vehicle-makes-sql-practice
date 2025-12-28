b43b9957512ebfcf122e378898e9f14f9a7eced5
Masa Sabobeh <masasabobeh03@gmail.com>
2025-12-29 00:28:51 +0200

    Add probelm 14 and 15

---
+++ problems/P14.sql
@@ -0,0 +1,4 @@
+--   Problem 14: Get all Makes with make starts with 'B'
+SELECT Make
+FROM Makes
+WHERE Make LIKE 'B%'
---
+++ problems/P15.sql
@@ -0,0 +1,4 @@
+--   Problem 15: Get all Makes with make ends with 'W'
+SELECT Make
+FROM Makes
+WHERE Make LIKE '%W'
